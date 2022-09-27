f67de607d521cad916f419d35b2a67070a9155f0
Nathan Schneider Gavenski <nathangavenski@gmail.com>
2022-09-27 18:53:03 -0300

    test script

---
+++ teste.py
@@ -0,0 +1,41 @@
+from datasets import load_dataset
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+
+class CelebA(Dataset):
+    def __init__(self) -> None:
+        super().__init__()
+        self.dataset = load_dataset("student/celebA")['train']
+
+    def __len__(self):
+        return self.dataset.num_rows
+    
+    def __getitem__(self, idx):
+        entry = self.dataset[idx]
+        image, label = entry['image'], entry['label']
+        image = transforms.ToTensor()(image)
+        label = torch.tensor(label)
+        return image, label
+
+
+if __name__ == '__main__':
+    dataloader = DataLoader(CelebA(), batch_size=4, shuffle=True)
+    model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False)
+    model.cuda()
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=5e-3)
+    for _ in range(100):
+        for is_train, _dataloader in zip([True, False], [dataloader, dataloader]):
+            for (image, label) in _dataloader:
+
+                if is_train:
+                    optimizer.zero_grad()
+                    prediction = model(image.cuda())
+                    argmax = torch.argmax(prediction, 1)
+                    loss = criterion(prediction, label.cuda())
+                    loss.backward()
+                else:
+                    model(image.cuda())
