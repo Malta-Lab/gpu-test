d91a39dfd55f3ae0949819c236eec55f76d5313a
Nathan Gavenski <nathangavenski@gmail.com>
2022-09-29 13:49:04 -0300

    fix for batch size

--- teste.py
+++ teste.py
@@ -1,8 +1,11 @@
+import os
+
 from datasets import load_dataset
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
+from tqdm import tqdm
 
 class CelebA(Dataset):
     def __init__(self) -> None:
@@ -21,15 +24,16 @@ class CelebA(Dataset):
 
 
 if __name__ == '__main__':
-    dataloader = DataLoader(CelebA(), batch_size=4, shuffle=True)
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    dataloader = DataLoader(CelebA(), batch_size=512, shuffle=True)
     model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False)
     model.cuda()
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=5e-3)
-    for _ in range(100):
+    for _ in tqdm(range(100)):
         for is_train, _dataloader in zip([True, False], [dataloader, dataloader]):
-            for (image, label) in _dataloader:
+            for (image, label) in tqdm(_dataloader):
 
                 if is_train:
                     optimizer.zero_grad()
